bdb763f0ea9e9ce5b3f9dd1a2a56e028e828076e
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2017-01-09 16:01:08 -0700

    Bug fix: Object#blank?

--- ext/faster_support/object/blank/object_blank.c
+++ ext/faster_support/object/blank/object_blank.c
@@ -27,14 +27,14 @@ static VALUE rb_obj_present(VALUE obj)
   return RB_NEGATE(rb_obj_blank(obj));
 }
 
-static VALUE rb_nil_blank(VALUE self)
-{
-  return Qtrue;
-}
-
-static VALUE rb_true_blank(VALUE self)
+static VALUE return_false()
 {
   return Qfalse;
+}
+
+static VALUE return_true()
+{
+  return Qtrue;
 }
 
 static VALUE rb_ary_blank(VALUE ary)
@@ -46,9 +46,19 @@ static VALUE rb_ary_blank(VALUE ary)
   return Qfalse;
 }
 
+static VALUE rb_ary_present(VALUE ary)
+{
+  return RB_NEGATE(rb_ary_blank(ary));
+}
+
 static VALUE rb_hash_blank(VALUE self)
 {
   return RHASH_EMPTY_P(self) ? Qtrue : Qfalse;
+}
+
+static VALUE rb_hash_present(VALUE self)
+{
+  return RB_NEGATE(rb_hash_blank(self));
 }
 
 void Init_blank(void)
@@ -58,14 +68,26 @@ void Init_blank(void)
   rb_define_method(rb_cObject, "blank?", rb_obj_blank, 0);
   rb_define_method(rb_cObject, "present?", rb_obj_present, 0);
 
-  rb_define_method(rb_cNilClass, "blank?", rb_nil_blank, 0);
-  rb_define_method(rb_cFalseClass, "blank?", rb_nil_blank, 0);
+  rb_define_method(rb_cNilClass, "blank?", return_true, 0);
+  rb_define_method(rb_cNilClass, "present?", return_false, 0);
+
+  rb_define_method(rb_cFalseClass, "blank?", return_true, 0);
+  rb_define_method(rb_cFalseClass, "present?", return_false, 0);
+
   rb_define_method(rb_cArray, "blank?", rb_ary_blank, 0);
+  rb_define_method(rb_cArray, "present?", rb_ary_present, 0);
+
   rb_define_method(rb_cHash, "blank?", rb_hash_blank, 0);
-  rb_define_method(rb_cTrueClass, "blank?", rb_true_blank, 0);
+  rb_define_method(rb_cHash, "present?", rb_hash_present, 0);
+
+  rb_define_method(rb_cTrueClass, "blank?", return_false, 0);
+  rb_define_method(rb_cTrueClass, "present?", return_true, 0);
 
   Init_str_blank();
 
-  rb_define_method(rb_cNumeric, "blank?", rb_true_blank, 0);
-  rb_define_method(rb_cTime, "blank?", rb_true_blank, 0);
+  rb_define_method(rb_cNumeric, "blank?", return_false, 0);
+  rb_define_method(rb_cNumeric, "present?", return_true, 0);
+
+  rb_define_method(rb_cTime, "blank?", return_false, 0);
+  rb_define_method(rb_cTime, "present?", return_true, 0);
 }
