d8a8edfd361d34e644ea008fff79de9b41145a4d
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2017-01-10 08:58:10 -0700

    Implement the :blank_implementation in Ruby.

--- lib/faster_support.rb
+++ lib/faster_support.rb
@@ -5,4 +5,25 @@ require 'faster_support/faster_support'
 require 'faster_support/core_ext'
 
 module FasterSupport
+  class << self
+    def blank_implementation
+      @blank_implementation ||= :active_support
+    end
+
+    def blank_implementation=(impl)
+      @blank_implementation =
+        case impl.to_sym
+        when :mri, :ruby then :mri
+        when :as, :active_support, :rails then :active_support
+        else
+          raise ArgumentError, "Unknown implementation: #{impl}. " \
+            'Please use :active_support or :mri.'
+        end
+    end
+
+    alias blank_impl blank_implementation
+    alias blank_impl= blank_implementation=
+  end
+
+  @blank_implementation = :active_support
 end
