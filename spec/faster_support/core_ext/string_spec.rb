c44392ee2ff000699c98e9c12f330aa49a9753bd
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2017-01-10 09:47:33 -0700

    Add specs for String#ascii_8bit

--- spec/faster_support/core_ext/string_spec.rb
+++ spec/faster_support/core_ext/string_spec.rb
@@ -1,4 +1,41 @@
 # frozen_string_literal: true
 
 RSpec.describe String do
+  describe '#ascii_8bit' do
+    let(:ascii) { Encoding::ASCII_8BIT }
+
+    context 'with a string that is already 8-bit ascii' do
+      let(:string) { 'hello'.encode(ascii) }
+
+      it 'returns self' do
+        expect(string.ascii_8bit).to be string
+      end
+    end
+
+    context 'with a frozen string' do
+      let(:string) { 'i am frozen' }
+      let(:newstr) { string.ascii_8bit }
+
+      it 'returns self with 8-bit encoding' do
+        expect(newstr.encoding).to eq ascii
+      end
+
+      it 'does not raise an error' do
+        expect { newstr }.not_to raise_error
+      end
+    end
+
+    context 'with a non-frozen string' do
+      let(:string) { String.new('hello') }
+      let(:newstr) { string.ascii_8bit }
+
+      it 'returns self with 8-bit encoding' do
+        expect(newstr.encoding).to eq ascii
+      end
+
+      it 'does not raise an error' do
+        expect { newstr }.not_to raise_error
+      end
+    end
+  end
 end
