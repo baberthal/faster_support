b6b0d159ded7f76beb1497d695e098d44d14529a
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2017-01-10 09:22:53 -0700

    Update extconf

--- ext/faster_support/core_ext/object/blank/extconf.rb
+++ ext/faster_support/core_ext/object/blank/extconf.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 require 'mkmf'
-# rubocop:disable all
+# rubocop:disable Style/GlobalVars
 $CPPFLAGS += "-I#{File.expand_path('../../../../', __FILE__)}"
 create_makefile('faster_support/core_ext/object/blank')
